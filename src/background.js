3eddedc9e4f953667a01ab0990532416dbdcd72b
Romario Torres <romariotorres@vivaldi.net>
2020-12-18 10:08:43 -0600

    Update background.js

--- src/background.js
+++ src/background.js
@@ -1,11 +1,15 @@
 'use strict'
 
-import { app, protocol, BrowserWindow, ipcMain } from 'electron'
-import { autoUpdater } from "electron-updater"
-import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
-import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
+import { app, protocol, BrowserWindow } from 'electron'
+import {
+  createProtocol,
+  installVueDevtools
+} from 'vue-cli-plugin-electron-builder/lib'
+import { autoUpdater } from 'electron-updater'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
 let win
 
 // Scheme must be registered before the app is ready
@@ -13,21 +17,19 @@ protocol.registerSchemesAsPrivileged([
   { scheme: 'app', privileges: { secure: true, standard: true } }
 ])
 
-async function createWindow() {
+function createWindow() {
   // Create the browser window.
   win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      // Use pluginOptions.nodeIntegration, leave this alone
-      // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: true
     }
   })
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
-    await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
+    win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
     if (!process.env.IS_TEST) win.webContents.openDevTools()
   } else {
     createProtocol('app')
@@ -36,10 +38,9 @@ async function createWindow() {
     autoUpdater.checkForUpdatesAndNotify()
   }
 
-  // win.on('closed', ()=>{
-  //   win = null
-  // })
-
+  win.on('closed', () => {
+    win = null
+  })
 }
 
 // Quit when all windows are closed.
@@ -54,7 +55,9 @@ app.on('window-all-closed', () => {
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  if (win === null) {
+    createWindow()
+  }
 })
 
 // This method will be called when Electron has finished
@@ -63,40 +66,24 @@ app.on('activate', () => {
 app.on('ready', async () => {
   if (isDevelopment && !process.env.IS_TEST) {
     // Install Vue Devtools
-    try {
-      await installExtension(VUEJS_DEVTOOLS)
-    } catch (e) {
-      console.error('Vue Devtools failed to install:', e.toString())
-    }
+    // Devtools extensions are broken in Electron 6.0.0 and greater
+    // See https://github.com/nklayman/vue-cli-plugin-electron-builder/issues/378 for more info
+    // Electron will not launch with Devtools extensions installed on Windows 10 with dark mode
+    // If you are not using Windows 10 dark mode, you may uncomment these lines
+    // In addition, if the linked issue is closed, you can upgrade electron and uncomment these lines
+    // try {
+    //   await installVueDevtools()
+    // } catch (e) {
+    //   console.error('Vue Devtools failed to install:', e.toString())
+    // }
   }
   createWindow()
 })
 
-
-ipcMain.on('app_version', (event)=>{
-  event.sender.send('app_version', {version: app.getVersion()})
-})
-
-
-
-autoUpdater.on('update-available', ()=>{
-  mainWindow.webContents.send('update_available')
-})
-
-autoUpdater.on('update-downloaded', ()=>{
-  mainWindow.webContents.send('update_downloaded')
-})
-
-ipcMain.on('restart_app', () => {
-  autoUpdater.quitAndInstall();
-});
-
-
-
 // Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
   if (process.platform === 'win32') {
-    process.on('message', (data) => {
+    process.on('message', data => {
       if (data === 'graceful-exit') {
         app.quit()
       }
@@ -107,5 +94,3 @@ if (isDevelopment) {
     })
   }
 }
-
-
