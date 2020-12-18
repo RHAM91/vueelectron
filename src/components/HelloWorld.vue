4141d90db6796f1d819d2ed9949a285bb469985f
Romario Torres <romariotorres@vivaldi.net>
2020-12-18 10:02:07 -0600

    fdgsdfg

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -19,7 +19,7 @@ export default {
     },
     methods: {
         do_action(){
-            this.info = 'Bienvenido a la version 1.3.4 re-buya!!!'
+            this.info = 'Bienvenido a la version 1.3.6 re-buya!!!'
         }
     },
 }
